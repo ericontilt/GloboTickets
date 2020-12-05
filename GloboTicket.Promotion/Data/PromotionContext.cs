8b0fef40561f2360ded1bf7e2f6bd3cd4bea1603
Michael L Perry <michael@qedcode.com>
2020-12-05 15:02:00 -0600

    Include child records so that consistency check works correctly

--- GloboTicket.Promotion/Data/PromotionContext.cs
+++ GloboTicket.Promotion/Data/PromotionContext.cs
@@ -69,6 +69,7 @@ namespace GloboTicket.Promotion.Data
         public async Task<Venue> GetOrInsertVenue(Guid venueGuid)
         {
             var venue = Venue
+                .Include(venue => venue.Descriptions)
                 .Where(venue => venue.VenueGuid == venueGuid)
                 .SingleOrDefault();
             if (venue == null)
